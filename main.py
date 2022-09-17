c73a47184a3c7a90e77d1f9bfb5ea51157ccd07c
leonor.perez.contractor.gdevtools.com <leonor.perez.contractor@gdevtools.com>
2022-09-17 18:27:24 -0300

    se cambia config y bbdd

--- main.py
+++ main.py
@@ -10,10 +10,17 @@ app = Flask(__name__)
 app.secret_key = 'your secret key'
 
  
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pythonlogin'
+#setup bbdd local
+#app.config['MYSQL_HOST'] = 'localhost'
+#app.config['MYSQL_USER'] = 'root'
+#app.config['MYSQL_PASSWORD'] = ''
+#app.config['MYSQL_DB'] = 'pythonlogin'
+
+#setup bbdd pythonanywhere
+app.config['MYSQL_HOST'] = 'leonorperez.mysql.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = 'leonorperez'
+app.config['MYSQL_PASSWORD'] = 'lascano4044'
+app.config['MYSQL_DB'] = 'leonorperez$pythonlogin'
 
 user = {}
 mysql = MySQL(app)
