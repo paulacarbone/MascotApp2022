bc93aa9453a91237601c76585c17a9372b204bc7
Paula Carbone <paulacamilacarbone@gmail.com>
2022-11-18 18:10:26 -0300

    Agregando pagina usuario

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ app.secret_key = 'your secret key'
 
  
 #setup bbdd local
-#app.config['MYSQL_HOST'] = 'localhost'
-#app.config['MYSQL_USER'] = 'root'
-#app.config['MYSQL_PASSWORD'] = ''
-#app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pythonlogin'
 
 #setup bbdd pythonanywhere
-app.config['MYSQL_HOST'] = 'leonorperez.mysql.pythonanywhere-services.com'
-app.config['MYSQL_USER'] = 'leonorperez'
-app.config['MYSQL_PASSWORD'] = 'lascano4044'
-app.config['MYSQL_DB'] = 'leonorperez$pythonlogin'
+#app.config['MYSQL_HOST'] = 'leonorperez.mysql.pythonanywhere-services.com'
+#app.config['MYSQL_USER'] = 'leonorperez'
+#app.config['MYSQL_PASSWORD'] = 'lascano4044'
+#app.config['MYSQL_DB'] = 'leonorperez$pythonlogin'
 
 user = {}
 mysql = MySQL(app)
@@ -207,8 +207,14 @@ def delete_publicacion(idPublicacion):
     return redirect(url_for('publicaciones'))
     
 
-#if __name__ == "__main__":
-#   app.run(port=3307, debug=True)
+@app.route('/usuario', methods=['GET'])
+def usuario():
+
+    return render_template('usuario.html', usuario = usuario)
+   
+
+if __name__ == "__main__":
+   app.run(port=3306, debug=True)
 
 
     
